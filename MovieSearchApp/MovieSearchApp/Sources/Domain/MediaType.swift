5e39f66a565cc60aa758eea86d972bbdb3f3ed4e
Grumpy <sewoong2890@gmail.com>
2022-12-16 13:37:04 +0900

    feat: MediaType, TimeWindow 구현

---
+++ MovieSearchApp/MovieSearchApp/Sources/Domain/MediaType.swift
@@ -0,0 +1,15 @@
+//
+//  MediaType.swift
+//  MovieSearchApp
+//
+//  Created by 박세웅 on 2022/12/16.
+//
+
+import Foundation
+
+enum MediaType: String {
+    case all
+    case movie
+    case tv
+    case person
+}
---
+++ MovieSearchApp/MovieSearchApp/Sources/Domain/TimeWindow.swift
@@ -0,0 +1,13 @@
+//
+//  TimeWindow.swift
+//  MovieSearchApp
+//
+//  Created by 박세웅 on 2022/12/16.
+//
+
+import Foundation
+
+enum TimeWindow: String {
+    case day
+    case week
+}
