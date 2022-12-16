cbb0385e8b764ad6ae23677f3e94078459b9e6d4
Grumpy <sewoong2890@gmail.com>
2022-12-17 03:59:12 +0900

    feat: APIRequestTests 테스트 코드 작성

---
+++ MovieSearchApp/MovieSearchAppTests/Network/APIRequestTests.swift
@@ -0,0 +1,46 @@
+//
+//  APIRequestTests.swift
+//  MovieSearchAppTests
+//
+//  Created by 박세웅 on 2022/12/16.
+//
+
+import XCTest
+import Quick
+import Nimble
+@testable import MovieSearchApp
+
+class APIRequestTests: QuickSpec {
+    
+    var provider: APIProvider?
+    var trending: Endpoint?
+    let decoder = JSONDecoder()
+    
+    override func spec() {
+        describe("API Provider를 사용하여 일일 영화 트렌드 API를 호출한다.") {
+            beforeEach {
+                self.provider = APIProvider()
+                self.trending = EndpointStorage.trendingAPI(.movie, .day).endpoint
+            }
+            context("Movie Trending API를 호출한다.") {
+                it("결과를 성공적으로 Decode 해야 한다.") {
+                    waitUntil(timeout: .seconds(2)) { [weak self] done in
+                        self?.provider?.request(endpoint: (self?.trending)!) { result in
+                            switch result {
+                            case .success(let data):
+                                let trending = try! self?.decoder.decode(Trending.self, from: data)
+                                
+                                expect(trending?.page).to(equal(1))
+                                expect(trending?.totalPages).to(equal(1000))
+                                expect(trending?.totalResults).to(equal(20000))
+                                done()
+                            case .failure(_):
+                                break
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
