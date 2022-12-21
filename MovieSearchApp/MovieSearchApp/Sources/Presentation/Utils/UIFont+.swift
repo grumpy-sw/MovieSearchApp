ec8467e02a746fcd3a200bac5502d3034ce43116
Grumpy <sewoong2890@gmail.com>
2022-12-21 16:28:39 +0900

    feat: UIFont Extension에 size, weight 변경하는 메서드 추가

---
+++ MovieSearchApp/MovieSearchApp/Sources/Presentation/Utils/UIFont+.swift
@@ -0,0 +1,17 @@
+//
+//  UIFont+.swift
+//  MovieSearchApp
+//
+//  Created by 박세웅 on 2022/12/21.
+//
+
+import UIKit
+
+extension UIFont {
+    static func preferredFont(for style: TextStyle, weight: Weight) -> UIFont {
+        let metrics = UIFontMetrics(forTextStyle: style)
+        let desc = UIFontDescriptor.preferredFontDescriptor(withTextStyle: style)
+        let font = UIFont.systemFont(ofSize: desc.pointSize, weight: weight)
+        return metrics.scaledFont(for: font)
+    }
+}
