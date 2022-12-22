da19ebfe59bf3b5c0928cc54ac2f746f714398cb
Grumpy <sewoong2890@gmail.com>
2022-12-22 17:55:06 +0900

    feat: DetailDescriptionView UI 구현

--- MovieSearchApp/MovieSearchApp/Sources/Presentation/MovieDetail/View/MovieDetailViewElements/DetailDescriptionView.swift
+++ MovieSearchApp/MovieSearchApp/Sources/Presentation/MovieDetail/View/MovieDetailViewElements/DetailDescriptionView.swift
@@ -10,7 +10,46 @@ import SnapKit
 import Then
 
 final class MovieDescriptionView: UIView {
-    let taglineLabel = UILabel()
-    let overViewStaticLabel = UILabel()
-    let overView = UILabel()
+    
+    let baseStackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.spacing = 15
+    }
+    let taglineLabel = UILabel().then {
+        $0.font = .preferredFont(forTextStyle: .body)
+        $0.textAlignment = .center
+    }
+    let overviewStaticLabel = UILabel().then {
+        $0.font = .preferredFont(forTextStyle: .headline)
+        $0.text = "Overview"
+    }
+    let overviewLabel = UILabel().then {
+        $0.font = .preferredFont(forTextStyle: .body)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setSubViews()
+        setLayoutConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension MovieDescriptionView {
+    private func setSubViews() {
+        baseStackView.addArrangedSubview(taglineLabel)
+        baseStackView.addArrangedSubview(overviewStaticLabel)
+        baseStackView.addArrangedSubview(overviewLabel)
+    }
+    
+    private func setLayoutConstraints() {
+        let spacing = CGFloat(20)
+        
+        baseStackView.snp.makeConstraints {
+            $0.directionalEdges.equalToSuperview().inset(spacing)
+        }
+    }
 }
