e326f1f4c4ac9742de1da1dae518abb4060be3c1
Grumpy <sewoong2890@gmail.com>
2022-12-22 18:23:57 +0900

    feat: 컬렉션 뷰 레이아웃 제외한 DetailCrewView UI 구현

--- MovieSearchApp/MovieSearchApp/Sources/Presentation/MovieDetail/View/MovieDetailViewElements/DetailCrewView.swift
+++ MovieSearchApp/MovieSearchApp/Sources/Presentation/MovieDetail/View/MovieDetailViewElements/DetailCrewView.swift
@@ -10,14 +10,27 @@ import SnapKit
 import Then
 
 final class DetailCrewView: UIView {
-    let productionStaticLabel = UILabel()
+    
+    private let baseStackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.spacing = 20
+    }
+    
     lazy var productionCollectionView = UICollectionView(frame: .zero, collectionViewLayout: createProductionLayout())
     
-    let castStaticLabel = UILabel()
     lazy var castCollectionView = UICollectionView(frame: .zero, collectionViewLayout: createCastLayout())
     
-    let crewStaticLabel = UILabel()
     lazy var crewCollectionView = UICollectionView(frame: .zero, collectionViewLayout: createCrewLayout())
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setSubViews()
+        setLayoutConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
 extension DetailCrewView {
@@ -31,5 +44,20 @@ extension DetailCrewView {
     
     private func createCrewLayout() -> UICollectionViewLayout {
         return UICollectionViewLayout.init()
+    }
+    
+    private func setSubViews() {
+        baseStackView.addArrangedSubview(productionCollectionView)
+        baseStackView.addArrangedSubview(castCollectionView)
+        baseStackView.addArrangedSubview(crewCollectionView)
+        addSubview(baseStackView)
+    }
+    
+    private func setLayoutConstraints() {
+        let spacing = CGFloat(20)
+        
+        baseStackView.snp.makeConstraints {
+            $0.directionalEdges.equalToSuperview().inset(spacing)
+        }
     }
 }
