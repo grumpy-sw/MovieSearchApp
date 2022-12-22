e0c775cd56c733e747839ae346c3daf8d7b61c46
Grumpy <sewoong2890@gmail.com>
2022-12-22 19:02:18 +0900

    feat: ProductionCompany 타입 생성

---
+++ MovieSearchApp/MovieSearchApp/Sources/Data/Network/DTO/ProductionCompany/ProductionCompany.swift
@@ -0,0 +1,18 @@
+//
+//  ProductionCompany.swift
+//  MovieSearchApp
+//
+//  Created by 박세웅 on 2022/12/22.
+//
+
+import Foundation
+
+struct ProductionCompanyDTO {
+    let name: String
+    let logoPath: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case logoPath = "logo_path"
+    }
+}
