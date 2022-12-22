12d9dbe941544789cb0a89cafe0d2113417855ae
Grumpy <sewoong2890@gmail.com>
2022-12-22 18:09:04 +0900

    feat: DetailStatusView UI 구현

--- MovieSearchApp/MovieSearchApp/Sources/Presentation/MovieDetail/View/MovieDetailViewElements/DetailStatusView.swift
+++ MovieSearchApp/MovieSearchApp/Sources/Presentation/MovieDetail/View/MovieDetailViewElements/DetailStatusView.swift
@@ -10,12 +10,81 @@ import SnapKit
 import Then
 
 final class DetailStatusView: UIView {
-    let statusStaticLabel = UILabel()
-    let statusLabel = UILabel()
-    let originalLanguageStaticLabel = UILabel()
-    let originalLanguageLabel = UILabel()
-    let budgetStaticLabel = UILabel()
-    let budgetLabel = UILabel()
-    let revenueStaticLabel = UILabel()
-    let revenueLabel = UILabel()
+    
+    private lazy var baseStackView = verticalStackView(20)
+    
+    private lazy var statusStackView = verticalStackView(5)
+    private lazy var statusStaticLabel = staticLabel("Status")
+    private lazy var statusLabel = variableLabel()
+    
+    private lazy var originalLanguageStackView = verticalStackView(5)
+    private lazy var originalLanguageStaticLabel = staticLabel("Original Language")
+    private lazy var originalLanguageLabel = variableLabel()
+    
+    private lazy var budgetStackView = verticalStackView(5)
+    private lazy var budgetStaticLabel = staticLabel("Budget")
+    private lazy var budgetLabel = variableLabel()
+    
+    private lazy var revenueStackView = verticalStackView(5)
+    private lazy var revenueStaticLabel = staticLabel("Revenue")
+    private lazy var revenueLabel = variableLabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setSubViews()
+        setLayoutConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension DetailStatusView {
+    
+    private func verticalStackView(_ spacing: CGFloat) -> UIStackView {
+        return UIStackView().then {
+            $0.axis = .vertical
+            $0.spacing = spacing
+            $0.distribution = .fillEqually
+        }
+    }
+    
+    private func staticLabel(_ text: String) -> UILabel {
+        return UILabel().then {
+            $0.text = text
+            $0.font = .preferredFont(for: .body, weight: .bold)
+        }
+    }
+    
+    private func variableLabel() -> UILabel {
+        return UILabel().then {
+            $0.font = .preferredFont(forTextStyle: .body)
+        }
+    }
+    
+    private func setSubViews() {
+        statusStackView.addArrangedSubview(statusStaticLabel)
+        statusStackView.addArrangedSubview(statusLabel)
+        originalLanguageStackView.addArrangedSubview(originalLanguageStaticLabel)
+        originalLanguageStackView.addArrangedSubview(originalLanguageLabel)
+        budgetStackView.addArrangedSubview(budgetStaticLabel)
+        budgetStackView.addArrangedSubview(budgetLabel)
+        revenueStackView.addArrangedSubview(revenueStaticLabel)
+        revenueStackView.addArrangedSubview(revenueLabel)
+        
+        baseStackView.addArrangedSubview(statusStackView)
+        baseStackView.addArrangedSubview(originalLanguageStackView)
+        baseStackView.addArrangedSubview(budgetStackView)
+        baseStackView.addArrangedSubview(revenueStackView)
+        
+        addSubview(baseStackView)
+    }
+    
+    private func setLayoutConstraints() {
+        let spacing = CGFloat(20)
+        baseStackView.snp.makeConstraints {
+            $0.directionalEdges.equalToSuperview().inset(spacing)
+        }
+    }
 }
