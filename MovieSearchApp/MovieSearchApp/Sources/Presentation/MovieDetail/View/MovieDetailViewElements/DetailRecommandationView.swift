bc87bca299e91f6b48dd42bd1ae938d1b012cd5a
Grumpy <sewoong2890@gmail.com>
2022-12-22 18:30:43 +0900

    feat: RecommandationView UI 구현

--- MovieSearchApp/MovieSearchApp/Sources/Presentation/MovieDetail/View/MovieDetailViewElements/DetailRecommandationView.swift
+++ MovieSearchApp/MovieSearchApp/Sources/Presentation/MovieDetail/View/MovieDetailViewElements/DetailRecommandationView.swift
@@ -10,12 +10,55 @@ import SnapKit
 import Then
 
 final class DetailRecommandationView: UIView {
-    let recommandationStaticLabel = UILabel()
     lazy var recommandationCollectionView = UICollectionView(frame: .zero, collectionViewLayout: createRecommandationLayout())
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setSubViews()
+        setLayoutConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
 extension DetailRecommandationView {
     private func createRecommandationLayout() -> UICollectionViewLayout {
-        return UICollectionViewLayout.init()
+        let sectionProvider = { (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+        
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            
+            let groupSize = NSCollectionLayoutSize(widthDimension: .absolute(300), heightDimension: .absolute(260))
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+            let section = NSCollectionLayoutSection(group: group)
+            section.orthogonalScrollingBehavior = .continuous
+            section.interGroupSpacing = 20
+            section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 20, bottom: 80, trailing: 20)
+            let titleSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(50))
+            let titleSupplementary = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: titleSize, elementKind: TitleSupplementaryView.titleElementKind, alignment: .topLeading)
+            
+            section.boundarySupplementaryItems = [titleSupplementary]
+            return section
+        }
+        
+        let config = UICollectionViewCompositionalLayoutConfiguration()
+        config.interSectionSpacing = 20
+
+        let layout = UICollectionViewCompositionalLayout(
+            sectionProvider: sectionProvider, configuration: config)
+        return layout
+    }
+    
+    private func setSubViews() {
+        addSubview(recommandationCollectionView)
+    }
+    
+    private func setLayoutConstraints() {
+        let spacing = CGFloat(20)
+        recommandationCollectionView.snp.makeConstraints {
+            $0.directionalEdges.equalToSuperview().inset(spacing)
+        }
     }
 }
