c79287e7a9e5d2e1c1a46657954a7b70fa8b47df
Grumpy <sewoong2890@gmail.com>
2022-12-24 02:43:29 +0900

    feat: Detail View Model에서 Image Load 호출 추가

--- MovieSearchApp/MovieSearchApp/Sources/Presentation/MovieDetail/ViewModel/MovieDetailViewModel.swift
+++ MovieSearchApp/MovieSearchApp/Sources/Presentation/MovieDetail/ViewModel/MovieDetailViewModel.swift
@@ -16,17 +16,37 @@ protocol MovieDetailViewModelInput {
 
 protocol MovieDetailViewModelOutput {
     var outputMovie: PublishRelay<MovieDetail> { get }
+    var posterImage: PublishRelay<Data?> { get }
+    var backdropImage: PublishRelay<Data?> { get }
 }
 
 protocol MovieDetailViewModelable: MovieDetailViewModelInput, MovieDetailViewModelOutput { }
 
 final class MovieDetailViewModel: MovieDetailViewModelable {
     
+    enum ImageKind {
+        case poster
+        case backdrop
+    }
+    
     private let movieDetailUseCase: MovieDetailUseCase
     private let movieId: Int
     private let decoder = JSONDecoder()
-
+    
     var outputMovie: PublishRelay<MovieDetail> = .init()
+    var posterImage: PublishRelay<Data?> = .init()
+    var backdropImage: PublishRelay<Data?> = .init()
+    
+    private var posterImagePath: String = "" {
+        didSet {
+            updatePosterImage(width: 500)
+        }
+    }
+    private var backdropImagePath: String = "" {
+        didSet {
+            updateBackdropImage(width: 780)
+        }
+    }
     
     init(_ movieId: Int, _ movieDetailUseCase: MovieDetailUseCase) {
         self.movieId = movieId
@@ -36,7 +56,12 @@ final class MovieDetailViewModel: MovieDetailViewModelable {
     func viewDidLoad() {
         fetchMovieDetails(by: movieId)
     }
-    func itemSelected(_ index: Int) { }
+    
+    
+    
+    func itemSelected(_ index: Int) {
+    
+    }
 }
 
 extension MovieDetailViewModel {
@@ -53,7 +78,41 @@ extension MovieDetailViewModel {
         }
     }
     
+    private func updatePosterImage(width: Int) {
+        fetchImageData(width: width, path: posterImagePath, kind: .poster)
+    }
+    
+    private func updateBackdropImage(width: Int) {
+        fetchImageData(width: width, path: backdropImagePath, kind: .backdrop)
+    }
+    
+    private func fetchImageData(width: Int, path: String?, kind: ImageKind) {
+        guard let path = path else {
+            return
+        }
+        
+        if path.isEmpty {
+            return
+        }
+        
+        _ = movieDetailUseCase.execute(width: width, path: path) { [weak self] result in
+            switch result {
+            case .success(let data):
+                if kind == .poster {
+                    self?.posterImage.accept(data)
+                } else {
+                    self?.backdropImage.accept(data)
+                }
+            case .failure(let error):
+                print(path)
+                print(error.errorDescription)
+            }
+        }
+    }
+    
     private func setObservableValues(_ movieDetails: MovieDetail) {
         outputMovie.accept(movieDetails)
+        posterImagePath = movieDetails.posterPath
+        backdropImagePath = movieDetails.backdropPath
     }
 }
