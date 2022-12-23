72919fe587a8e833e908e1c3cf0e4c41e286e5d4
Grumpy <sewoong2890@gmail.com>
2022-12-24 02:39:37 +0900

    feat: (Detail) BackdropView CastView ProductionView 생성

---
+++ MovieSearchApp/MovieSearchApp/Sources/Presentation/MovieDetail/View/MovieDetailViewElements/DetailBackdropView.swift
@@ -0,0 +1,42 @@
+//
+//  DetailBackdropView.swift
+//  MovieSearchApp
+//
+//  Created by 박세웅 on 2022/12/24.
+//
+
+import UIKit
+import SnapKit
+
+final class DetailBackdropView: UIView {
+    
+    private let backdropImageView: UIImageView = UIImageView()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setSubViews()
+        setLayoutConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension DetailBackdropView {
+    private func setSubViews() {
+        addSubview(backdropImageView)
+    }
+    
+    private func setLayoutConstraints() {
+        backdropImageView.snp.makeConstraints {
+            $0.directionalEdges.equalToSuperview()
+        }
+    }
+    
+    func updateBackdropImage(_ image: Data?) {
+        guard let image = image else {
+            return
+        }
+        backdropImageView.image = UIImage(data: image)
+    }
+}
---
+++ MovieSearchApp/MovieSearchApp/Sources/Presentation/MovieDetail/View/MovieDetailViewElements/DetailProductionView.swift
@@ -0,0 +1,71 @@
+//
+//  DetailProductionView.swift
+//  MovieSearchApp
+//
+//  Created by 박세웅 on 2022/12/24.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class DetailProductionView: UIView {
+    
+    lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setSubViews()
+        setLayoutConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension DetailProductionView {
+    private func createLayout() -> UICollectionViewLayout {
+        let sectionProvider = { (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            
+            let groupSize = NSCollectionLayoutSize(widthDimension: .absolute(170), heightDimension: .absolute(150))
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+            
+            let section = NSCollectionLayoutSection(group: group)
+            section.orthogonalScrollingBehavior = .continuous
+            section.interGroupSpacing = 20
+            section.contentInsets = NSDirectionalEdgeInsets(top: 30, leading: 20, bottom: 30, trailing: 20)
+            
+            let titleSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(50))
+            let titleSupplementary = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: titleSize, elementKind: TitleSupplementaryView.titleElementKind, alignment: .topLeading)
+            
+            section.boundarySupplementaryItems = [titleSupplementary]
+            return section
+        }
+        
+        let config = UICollectionViewCompositionalLayoutConfiguration()
+        config.interSectionSpacing = 20
+        
+        let layout = UICollectionViewCompositionalLayout(
+            sectionProvider: sectionProvider, configuration: config)
+        return layout
+    }
+    
+    private func setSubViews() {
+        addSubview(collectionView)
+    }
+    
+    private func setLayoutConstraints() {
+        collectionView.snp.makeConstraints {
+            $0.directionalEdges.equalToSuperview()
+        }
+    }
+    
+    func setContent(_ productionCompanies: [ProductionCompany], _ credits: Credits) {
+        
+    }
+}
+
