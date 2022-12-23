df485381b68b8d4859598d9474f8de827cef7b0b
Grumpy <sewoong2890@gmail.com>
2022-12-24 02:40:44 +0900

    feat: UseCase에 이미지 로드 메서드 추가

--- MovieSearchApp/MovieSearchApp/Sources/Domain/UseCases/MovieDetailUseCase.swift
+++ MovieSearchApp/MovieSearchApp/Sources/Domain/UseCases/MovieDetailUseCase.swift
@@ -9,6 +9,7 @@ import Foundation
 
 protocol MovieDetailUseCase {
     func execute(id: Int, completion: @escaping (Result<Data, NetworkError>) -> Void) -> URLSessionDataTask?
+    func execute(width: Int, path: String, completion: @escaping (Result<Data, NetworkError>) -> Void) -> URLSessionDataTask?
 }
 
 final class DefaultMovieDetailUseCase: MovieDetailUseCase {
@@ -20,6 +21,12 @@ final class DefaultMovieDetailUseCase: MovieDetailUseCase {
     
     func execute(id: Int, completion: @escaping (Result<Data, NetworkError>) -> Void) -> URLSessionDataTask? {
         return movieDetailRepository.fetchMovieDetails(id: id) { result in
+            completion(result)
+        }
+    }
+    
+    func execute(width: Int, path: String, completion: @escaping (Result<Data, NetworkError>) -> Void) -> URLSessionDataTask? {
+        return movieDetailRepository.fetchImageData(width: width, path: path) { result in
             completion(result)
         }
     }
