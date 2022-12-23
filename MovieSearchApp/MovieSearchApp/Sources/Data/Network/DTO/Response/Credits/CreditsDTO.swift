3a554b755aecaf4b146fcb54d7856ce1c9339be4
Grumpy <sewoong2890@gmail.com>
2022-12-23 19:39:51 +0900

    feat: 같은 이름으로 여러 job을 가진 Crew를 하나로 묶어서 전달하도록 recombine 메서드 구현

--- MovieSearchApp/MovieSearchApp/Sources/Data/Network/DTO/Response/Credits/CreditsDTO.swift
+++ MovieSearchApp/MovieSearchApp/Sources/Data/Network/DTO/Response/Credits/CreditsDTO.swift
@@ -16,7 +16,7 @@ extension CreditsDTO {
     func toDomain() -> Credits {
         return Credits(
             cast: slice(cast).map { $0.toDomain() },
-            crew: slice(filterCrew()).map { $0.toDomain() })
+            crew: slice(recombine(filterCrew())).map { $0.toDomain() })
     }
     
     private func slice<T>(_ collection: [T]) -> [T] {
@@ -29,5 +29,23 @@ extension CreditsDTO {
     private func filterCrew() -> [CrewDTO] {
         let showingJobs = ["Director", "Story", "Writer", "ScreenPlay", "Writer"]
         return crew.filter { showingJobs.contains($0.job ?? "") }
+    }
+    
+    func recombine(_ crew: [CrewDTO]) -> [CrewDTO] {
+        var dictionary = [String:[String]]()
+        
+        for member in crew {
+            guard let name = member.name, let job = member.job else {
+                continue
+            }
+            
+            if let jobs = dictionary[name] {
+                dictionary[name] = jobs + [job]
+            } else {
+                dictionary[name] = [job]
+            }
+        }
+
+        return dictionary.map { CrewDTO(name: $0.key, job:$0.value.joined(separator: ",")) }
     }
 }
