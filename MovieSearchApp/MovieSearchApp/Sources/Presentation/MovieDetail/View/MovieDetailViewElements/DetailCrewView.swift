05432ad5fbd3a21ecbc72d315040558bf8f7e925
Grumpy <sewoong2890@gmail.com>
2022-12-23 19:43:10 +0900

    feat: DetailCrewView의 컬렉션 뷰 레이아웃 만드는 함수 구현

--- MovieSearchApp/MovieSearchApp/Sources/Presentation/MovieDetail/View/MovieDetailViewElements/DetailCrewView.swift
+++ MovieSearchApp/MovieSearchApp/Sources/Presentation/MovieDetail/View/MovieDetailViewElements/DetailCrewView.swift
@@ -11,7 +11,11 @@ import Then
 
 final class DetailCrewView: UIView {
     
-    lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+    lazy var productionCollectionView = UICollectionView(frame: .zero, collectionViewLayout: createProductionLayout())
+    
+    lazy var castCollectionView = UICollectionView(frame: .zero, collectionViewLayout: createCastLayout())
+    
+    lazy var crewCollectionView = UICollectionView(frame: .zero, collectionViewLayout: createCrewLayout())
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -25,20 +29,20 @@ final class DetailCrewView: UIView {
 }
 
 extension DetailCrewView {
-    private func createLayout() -> UICollectionViewLayout {
+    private func createProductionLayout() -> UICollectionViewLayout {
         let sectionProvider = { (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
         
             let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
             let item = NSCollectionLayoutItem(layoutSize: itemSize)
             
-            let groupSize = NSCollectionLayoutSize(widthDimension: .absolute(120), heightDimension: .absolute(300))
+            let groupSize = NSCollectionLayoutSize(widthDimension: .absolute(170), heightDimension: .absolute(150))
             let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
             let section = NSCollectionLayoutSection(group: group)
             section.orthogonalScrollingBehavior = .continuous
             section.interGroupSpacing = 20
             section.contentInsets = NSDirectionalEdgeInsets(top: 30, leading: 20, bottom: 80, trailing: 20)
             let titleSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(50))
-            let titleSupplementary = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: titleSize, elementKind: RecommendationSupplementaryView.recommendationElementKind, alignment: .topLeading)
+            let titleSupplementary = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: titleSize, elementKind: TitleSupplementaryView.titleElementKind, alignment: .topLeading)
             
             section.boundarySupplementaryItems = [titleSupplementary]
             return section
@@ -52,14 +56,77 @@ extension DetailCrewView {
         return layout
     }
     
+    private func createCastLayout() -> UICollectionViewLayout {
+        let sectionProvider = { (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+        
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            
+            let groupSize = NSCollectionLayoutSize(widthDimension: .absolute(120), heightDimension: .absolute(300))
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+            let section = NSCollectionLayoutSection(group: group)
+            section.orthogonalScrollingBehavior = .continuous
+            section.interGroupSpacing = 20
+            section.contentInsets = NSDirectionalEdgeInsets(top: 30, leading: 20, bottom: 80, trailing: 20)
+            let titleSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(50))
+            let titleSupplementary = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: titleSize, elementKind: TitleSupplementaryView.titleElementKind, alignment: .topLeading)
+            
+            section.boundarySupplementaryItems = [titleSupplementary]
+            return section
+        }
+        
+        let config = UICollectionViewCompositionalLayoutConfiguration()
+        config.interSectionSpacing = 20
+
+        let layout = UICollectionViewCompositionalLayout(
+            sectionProvider: sectionProvider, configuration: config)
+        return layout
+    }
+    
+    private func createCrewLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(50))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 2)
+        let spacing = CGFloat(10)
+        group.interItemSpacing = .fixed(spacing)
+
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = spacing
+        section.contentInsets = NSDirectionalEdgeInsets(top: 30, leading: 10, bottom: 0, trailing: 10)
+        let titleSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(50))
+        let titleSupplementary = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: titleSize, elementKind: TitleSupplementaryView.titleElementKind, alignment: .topLeading)
+        
+        section.boundarySupplementaryItems = [titleSupplementary]
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+    
     private func setSubViews() {
-        addSubview(collectionView)
+        addSubview(productionCollectionView)
+        addSubview(castCollectionView)
+        addSubview(crewCollectionView)
     }
     
     private func setLayoutConstraints() {
         let spacing = CGFloat(20)
         
-        collectionView.snp.makeConstraints {
+//        productionCollectionView.snp.makeConstraints {
+//            $0.top.leading.trailing.equalToSuperview()
+//        }
+//
+//        castCollectionView.snp.makeConstraints {
+//            $0.leading.trailing.equalToSuperview()
+//            $0.top.equalTo(productionCollectionView.snp.bottom).inset(spacing)
+//        }
+//
+//        crewCollectionView.snp.makeConstraints {
+//            $0.leading.trailing.equalToSuperview()
+//            $0.top.equalTo(castCollectionView.snp.bottom).inset(spacing)
+//            $0.bottom.equalToSuperview()
+//        }
+        crewCollectionView.snp.makeConstraints {
             $0.directionalEdges.equalToSuperview()
         }
     }
