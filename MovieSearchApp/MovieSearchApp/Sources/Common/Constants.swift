5128051ceabd100a8302a044fc96f4d3da1c093d
Grumpy <sewoong2890@gmail.com>
2022-12-26 14:55:44 +0900

    refactor: 사용하지 않는 전역상수 제거

--- MovieSearchApp/MovieSearchApp/Sources/Common/Constants.swift
+++ MovieSearchApp/MovieSearchApp/Sources/Common/Constants.swift
@@ -9,7 +9,6 @@ import Foundation
 
 enum Constants {
     static let apiKey = "c34bb382e26143a054947d411d07d1a1"
-    static let imageURL = "https://image.tmdb.org/t/p"
     static let appendToResponse = ["recommendations", "credits"]
     
     static let errorAlertTitle = "API Network Error"
